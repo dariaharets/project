084bbe241f6feff2bfce678f1ae5170d14c24c7e
Daria Harets <dariaharetc@gmail.com>
2021-08-11 14:34:18 +0300

    formatted doc

--- src/components/Section3.vue
+++ src/components/Section3.vue
@@ -1,47 +1,79 @@
 <template>
-        <div class="section3">
-            <div class="section3__text">
-                <h2>Buy a set with a <strong class="yellow-text">30%</strong> discount</h2>
-                <p>These baby filters make your drone look faster. Or at least you won’t lose them like those other
-                    black filters</p>
-            </div>
-            <div class="section3__slider">
-                <div class="section3__slider-slide">
-                    <img class="section3__slider-image" alt="sorry" src="../assets/slide1.png"/>
-                    <h3>Fire engine red filters set</h3>
-                    <p>These baby filters make your drone look faster. Or at least you won’t
-                        lose them like those other black filters</p>
-                        <p class="price">240 USD
-                        <span class="decorated">260 USD</span>
-                        </p>
-                    <button class="primary-button slide-button">Buy</button>
-                </div>
-                <div class="section3__slider-slide">
-                    <img class="section3__slider-image" alt="sorry" src="../assets/slide2.png"/>
-                    <h3>Attach ND filters</h3>
-                    <p>Not only do these filters work seamlessly with the camera mounts on both Mavic 2 Drones...</p>
-                        <p class="price">370 USD</p>
-                    <button class="primary-button slide-button">Buy</button>
-                </div>
-                <div class="section3__slider-slide">
-                    <img class="section3__slider-image" alt="sorry" src="../assets/slide3.png"/>
-                    <h3>Drone landing pad</h3>
-                    <p>The helipad that does more for you. Like a changing mat for your surf session, the landing pad acts as...</p>
-                        <p class="price">228 USD
-                        </p>
-                    <button class="primary-button slide-button">Buy</button>
-                </div>
-                <div class="section3__slider-slide">
-                    <img class="section3__slider-image" alt="sorry" src="../assets/slide4.png"/>
-                    <h3>Ultra thin photo case</h3>
-                    <p>Inspired by filmmakers, drone flyers, and minimalists this is the case you don’t have to take off to use.</p>
-                        <p class="price">183 USD
-                        <span class="decorated">210 USD</span>
-                        </p>
-                    <button class="primary-button slide-button">Buy</button>
-                </div>
-            </div>
-        </div>
+  <div class="section3">
+    <div class="section3__text">
+      <h2>
+        Buy a set with a <strong class="yellow-text">30%</strong> discount
+      </h2>
+      <p>
+        These baby filters make your drone look faster. Or at least you won’t
+        lose them like those other black filters
+      </p>
+    </div>
+    <div class="section3__slider">
+      <div class="section3__slider-slide">
+        <img
+          class="section3__slider-image"
+          alt="sorry"
+          src="../assets/slide1.png"
+        />
+        <h3>Fire engine red filters set</h3>
+        <p>
+          These baby filters make your drone look faster. Or at least you won’t
+          lose them like those other black filters
+        </p>
+        <p class="price">
+          240 USD
+          <span class="decorated">260 USD</span>
+        </p>
+        <button class="primary-button slide-button">Buy</button>
+      </div>
+      <div class="section3__slider-slide">
+        <img
+          class="section3__slider-image"
+          alt="sorry"
+          src="../assets/slide2.png"
+        />
+        <h3>Attach ND filters</h3>
+        <p>
+          Not only do these filters work seamlessly with the camera mounts on
+          both Mavic 2 Drones...
+        </p>
+        <p class="price">370 USD</p>
+        <button class="primary-button slide-button">Buy</button>
+      </div>
+      <div class="section3__slider-slide">
+        <img
+          class="section3__slider-image"
+          alt="sorry"
+          src="../assets/slide3.png"
+        />
+        <h3>Drone landing pad</h3>
+        <p>
+          The helipad that does more for you. Like a changing mat for your surf
+          session, the landing pad acts as...
+        </p>
+        <p class="price">228 USD</p>
+        <button class="primary-button slide-button">Buy</button>
+      </div>
+      <div class="section3__slider-slide">
+        <img
+          class="section3__slider-image"
+          alt="sorry"
+          src="../assets/slide4.png"
+        />
+        <h3>Ultra thin photo case</h3>
+        <p>
+          Inspired by filmmakers, drone flyers, and minimalists this is the case
+          you don’t have to take off to use.
+        </p>
+        <p class="price">
+          183 USD
+          <span class="decorated">210 USD</span>
+        </p>
+        <button class="primary-button slide-button">Buy</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
